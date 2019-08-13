8eb5d1261ad0c73c9fbff0d618e72c825b2d986b
devin-bielejec <devin.bieleje@gmail.com>
2019-08-13 17:28:02 -0400

    Movie list is a link

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,24 +3,23 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
- 
+
   useEffect(() => {
     const id = props.match.params.id;
-    console.log(id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+    axios
+    .get(`http://localhost:5000/api/movies/${id}`)
+    .then(response => {
+      console.log(response);
+      setMovie(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+  }, []);
 
-  },[movie]);
-  
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -30,6 +29,8 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+
+  console.log(movie, "37");
 
   const { title, director, metascore, stars } = movie;
   return (
