2496bfd840f4d145c9b24cf5ebbbd804e1500fbb
devin-bielejec <devin.bieleje@gmail.com>
2019-08-14 12:36:46 -0400

    Saved movies work

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  console.log(props);
 
   useEffect(() => {
-    console.log("in this effect");
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
@@ -13,7 +13,6 @@ const Movie = (props) => {
       axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(response => {
-        console.log(response);
         setMovie(response.data);
       })
       .catch(error => {
@@ -25,11 +24,10 @@ const Movie = (props) => {
 
   }, [props.match.params.id]);
 
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -56,7 +54,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={()=>saveMovie()}>Save</div>
     </div>
   );
 }
